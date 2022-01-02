6f37a52892ed303a3307e30411f6a9576cc4e766
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-02 22:28:44 +0100

    maps

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-var score = 99.5
+import "fmt"
 
 func main() {
-	sayHello("World")
 
-	for _, point := range points {
-		println(point)
+	menu := map[string]float64{
+		"Hamburger":    2.5,
+		"Cheeseburger": 3.5,
+		"Hotdog":       2.0,
+		"Fries":        1.5,
+		"Drink":        1.0,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["Hamburger"])
+
+	// looping maps
+	for key, value := range menu {
+		fmt.Println(key, "-", value)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		1: "John",
+		2: "Jane",
+		3: "Jack",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[1])
+
+	phonebook[2] = "Jill"
+
+	fmt.Println(phonebook)
 
 }
