e5975d6e24346a72bd647da329286bb324511541
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-04 10:15:22 +0100

    parsing floats

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,22 +25,61 @@ func createBill() bill {
 	return b
 }
 
-func promptOptions(b bill) {
+func addItem(b *bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	item := getInput("Enter item name: ", reader)
+	price := getInput("Enter item price: ", reader)
+
+	// convert price to float
+	priceFloat, err := strconv.ParseFloat(price, 64)
+
+	if err != nil {
+		fmt.Println("Invalid price")
+		return
+	}
+
+	b.addItem(item, priceFloat)
+}
+
+func updateTip(b *bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	tip := getInput("Enter tip: ", reader)
+
+	// convert tip to float
+	tipFloat, err := strconv.ParseFloat(tip, 64)
+
+	if err != nil {
+		fmt.Println("Invalid tip")
+		return
+	}
+
+	b.updateTip(tipFloat)
+}
+
+func promptOptions(b *bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("Options:")
 	fmt.Println("\t1. Add item")
 	fmt.Println("\t2. Update tip")
-	fmt.Println("\t3. Exit")
+	fmt.Println("\t3. Print bill")
+	fmt.Println("\t4. Exit")
 
 	input := getInput("Enter your choice: ", reader)
 
 	switch input {
 	case "1":
-		// addItem(b)
+		addItem(b)
+		promptOptions(b)
 	case "2":
-		// updateTip(b)
+		updateTip(b)
+		promptOptions(b)
 	case "3":
+		fmt.Println(b.format())
+		promptOptions(b)
+	case "4":
 		fmt.Println("Exiting...")
 		os.Exit(0)
 	default:
@@ -51,6 +91,6 @@ func promptOptions(b bill) {
 func main() {
 
 	myBill := createBill()
-	promptOptions(myBill)
+	promptOptions(&myBill)
 
 }
