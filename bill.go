e53c178133f56720c64e2368f20b3a7fc2a754c9
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-04 09:44:50 +0100

    receiver function with pointer

--- bill.go
+++ bill.go
@@ -11,33 +11,40 @@ type bill struct {
 // make new bills
 func newBill(name string) bill {
 	b := bill{
-		name: name,
-		items: map[string]float64{
-			"Food":   10.00,
-			"Drinks": 5.00,
-		},
-		tip: 0,
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
 	}
 	return b
 }
 
 // format the bill
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "\tBill breakdown:\n"
 	var total float64 = 0
 
 	// list items
 	for k, v := range b.items {
-		fs += fmt.Sprintf("%-25s: $%.2f\n", k, v)
+		fs += fmt.Sprintf("%-25s $%.2f\n", k+":", v)
 		total += v
 	}
 
 	// add tip
-	fs += fmt.Sprintf("\tTip: $%.2f\n", b.tip)
+	fs += fmt.Sprintf("\t%-10s $%.2f\n", "Tip:", b.tip)
 	total += b.tip
 
 	// add total
-	fs += fmt.Sprintf("\tTotal: $%.2f\n", total)
+	fs += fmt.Sprintf("\t%-10s $%.2f\n", "Total:", total)
 
 	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+// add item to bill
+func (b *bill) addItem(item string, price float64) {
+	b.items[item] = price
 }
--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func main() {
 
 	myBill := newBill("Omar's Bill")
 
+	myBill.addItem("Coffee", 2.50)
+	myBill.addItem("Cake", 5.00)
+	myBill.addItem("Chips", 1.00)
+
+	myBill.updateTip(.20)
+
 	fmt.Println(myBill.format())
 
 }
