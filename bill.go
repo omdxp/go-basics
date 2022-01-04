d3b0be1d0efbb95717461ecbcfc87a0b4768572e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-04 09:35:39 +0100

    receiver function

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -9,9 +11,33 @@ type bill struct {
 // make new bills
 func newBill(name string) bill {
 	b := bill{
-		name:  name,
-		items: map[string]float64{},
-		tip:   0,
+		name: name,
+		items: map[string]float64{
+			"Food":   10.00,
+			"Drinks": 5.00,
+		},
+		tip: 0,
 	}
 	return b
+}
+
+// format the bill
+func (b bill) format() string {
+	fs := "\tBill breakdown:\n"
+	var total float64 = 0
+
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25s: $%.2f\n", k, v)
+		total += v
+	}
+
+	// add tip
+	fs += fmt.Sprintf("\tTip: $%.2f\n", b.tip)
+	total += b.tip
+
+	// add total
+	fs += fmt.Sprintf("\tTotal: $%.2f\n", total)
+
+	return fs
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 
 	myBill := newBill("Omar's Bill")
-	fmt.Println(myBill)
+
+	fmt.Println(myBill.format())
 
 }
