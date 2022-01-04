26f09b75d6b84f82f69002db32eccb77d9590c03
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-04 10:03:42 +0100

    user input

--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) string {
+	fmt.Print(prompt)
+	input, _ := r.ReadString('\n')
+	return strings.TrimSpace(input)
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill:", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Options:")
+	fmt.Println("\t1. Add item")
+	fmt.Println("\t2. Update tip")
+	fmt.Println("\t3. Exit")
+
+	input := getInput("Enter your choice: ", reader)
+
+	switch input {
+	case "1":
+		// addItem(b)
+	case "2":
+		// updateTip(b)
+	case "3":
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid input")
+		promptOptions(b)
+	}
+}
 
 func main() {
 
-	myBill := newBill("Omar's Bill")
-
-	myBill.addItem("Coffee", 2.50)
-	myBill.addItem("Cake", 5.00)
-	myBill.addItem("Chips", 1.00)
-
-	myBill.updateTip(.20)
-
-	fmt.Println(myBill.format())
+	myBill := createBill()
+	promptOptions(myBill)
 
 }
